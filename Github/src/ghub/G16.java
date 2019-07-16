d5459edd8017eac5999639e4ce8ae154a30745b1
VAIBHAV <VAIBHAV@192.168.11.140>
2019-07-16 15:14:37 +0530

    16/7 3:14

---
+++ Github/src/ghub/G16.java
@@ -0,0 +1,29 @@
+package ghub;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class G16 {
+	WebDriver driver;
+
+	@Test
+  public void f() {
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "D:\\158\\chromedriver.exe");
+      driver=new ChromeDriver();
+	   
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+  }
+
+}
